4298264a6eb0ea5994e9021894369c8c39a88ff1
Andrew Davis <atd285@gmail.com>
2019-04-13 14:47:48 -0400

    add languages nav component and prop-types

--- app/Components/Popular.js
+++ app/Components/Popular.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+function LanguagesNav({ selectedLanguage, updateLanguage }) {
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+  return (
+    <ul className='flex-center'>
+      {languages.map((language) => (
+        <li>
+          <button
+            className='btn-clear nav-link'
+            type='button'
+            onClick={() => updateLanguage(language)}
+            style={
+              language === selectedLanguage
+                ? { color: 'rgb(187, 46, 31)' }
+                : null
+            }
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 class Popular extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedLanguage: null,
-    };
-  }
+  state = {
+    selectedLanguage: null,
+  };
 
   updateLanguage = (selectedLang) => {
     this.setState(() => ({
@@ -15,26 +37,21 @@ class Popular extends React.Component {
   };
 
   render() {
-    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
     const { selectedLanguage } = this.state;
     return (
-      <ul className='flex-center'>
-        {languages.map((lang, index) => (
-          <li>
-            <button
-              type='button'
-              className='btn-clear nav-link'
-              onClick={() => this.updateLanguage(lang)}
-              style={
-                lang === selectedLanguage ? { color: 'rgb(187, 46, 31)' } : null
-              }
-            >
-              {lang}
-            </button>
-          </li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <LanguagesNav
+          selectedLanguage={selectedLanguage}
+          updateLanguage={this.updateLanguage}
+        />
+      </React.Fragment>
     );
   }
 }
+
+LanguagesNav.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  updateLanguage: PropTypes.func.isRequired,
+};
+
 export default Popular;
