8ed58e93e7d1851f99d3397646ff116f80e4e301
Andrew Davis <atd285@gmail.com>
2019-04-19 21:00:08 -0400

    results UI setup

--- app/Components/Results.js
+++ app/Components/Results.js
@@ -11,23 +11,35 @@ class Results extends React.Component {
 
   componentDidMount() {
     const { playerOne, playerTwo } = this.props;
-    battle([playerOne, playerTwo]).then((results) => {
-      this.setState(() => ({
-        winner: results[0],
-        loser: results[1],
-        loading: false,
-      }));
-    });
+    battle([playerOne, playerTwo])
+      .then((results) => {
+        console.log(results);
+        this.setState(() => ({
+          winner: results[0],
+          loser: results[1],
+          loading: false,
+          error: false,
+        }));
+      })
+      .catch((error) => {
+        this.setState(() => ({
+          error,
+          loading: false,
+        }));
+      });
   }
 
   render() {
+    const { winner, loser } = this.state;
+    if (this.state.loading === true) <h1>LOADING...</h1>;
     return (
-      <div>
-        {this.state.loading ? (
-          <h1>LOADING</h1>
-        ) : (
-          <pre>{JSON.stringify(this.state, null, 2)}</pre>
-        )}
+      <div className='grid space-around container-sm'>
+        <div className='card bg-light'>
+          <h4 className='header-lg center-text'>
+            {winner.score === loser.score ? 'Tie' : 'Winner'}
+          </h4>
+          <img src={winner.profile.avatar_url} className='avatar' />
+        </div>
       </div>
     );
   }
