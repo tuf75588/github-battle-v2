5265989a348b8adc48776b78d4849867049c0b75
Andrew Davis <atd285@gmail.com>
2019-08-23 19:07:39 -0400

    convert battle component to hooks

--- app/Components/PlayerInput.js
+++ app/Components/PlayerInput.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ThemeConsumer } from '../contexts/theme';
 
 class PlayerInput extends React.Component {
   state = {
@@ -14,6 +13,7 @@ class PlayerInput extends React.Component {
 
   handleInputChange = (event) => {
     const { value } = event.target;
+
     this.setState(() => ({
       username: value,
     }));
@@ -21,33 +21,29 @@ class PlayerInput extends React.Component {
 
   render() {
     return (
-      <ThemeConsumer>
-        {({ theme }) => (
-          <form className='column player' onSubmit={this.handleSubmit}>
-            <label htmlFor='username' className='player-label'>
-              {this.props.label}
-            </label>
-            <div className='row player-inputs'>
-              <input
-                type='text'
-                id='username'
-                className={`input-${theme}`}
-                placeholder='github username'
-                value={this.state.username}
-                autoComplete='off'
-                onChange={this.handleInputChange}
-              />
-              <button
-                type='submit'
-                className={`btn ${theme === 'dark' ? 'light-btn' : 'dark-btn'}`}
-                disabled={!this.state.username}
-              >
-                Submit
-              </button>
-            </div>
-          </form>
-        )}
-      </ThemeConsumer>
+      <form className='column player' onSubmit={this.handleSubmit}>
+        <label htmlFor='username' className='player-label'>
+          {this.props.label}
+        </label>
+        <div className='row player-inputs'>
+          <input
+            type='text'
+            id='username'
+            className='input'
+            placeholder='github username'
+            value={this.state.username}
+            autoComplete='off'
+            onChange={this.handleInputChange}
+          />
+          <button
+            type='submit'
+            // className={`btn ${theme === 'dark' ? 'light-btn' : 'dark-btn'}`}
+            disabled={!this.state.username}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
     );
   }
 }
--- app/Components/PlayerPreview.js
+++ app/Components/PlayerPreview.js
@@ -1,32 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FaTimesCircle } from 'react-icons/fa';
-import { ThemeConsumer } from '../contexts/theme';
 
 function PlayerPreview({ username, onReset, label }) {
   return (
-    <ThemeConsumer>
-      {({ theme }) => (
-        <div className='column player'>
-          <h3 className='player-label'>{label}</h3>
-          <div className={`row bg-${theme}`}>
-            <div className='player-info'>
-              <img
-                src={`https://www.github.com/${username}.png?size=200`}
-                alt={`avatar for ${username}`}
-                className='avatar-small'
-              />
-              <a href={`https://www.github.com/${username}`} className='link'>
-                {username}
-              </a>
-            </div>
-            <button className='btn-clear flex-center' onClick={onReset}>
-              <FaTimesCircle size={26} color='rgb(194,57,42)' />
-            </button>
-          </div>
+    <div className='column player'>
+      <h3 className='player-label'>{label}</h3>
+      <div className='row'>
+        <div className='player-info'>
+          <img
+            src={`https://www.github.com/${username}.png?size=200`}
+            alt={`avatar for ${username}`}
+            className='avatar-small'
+          />
+          <a href={`https://www.github.com/${username}`} className='link'>
+            {username}
+          </a>
         </div>
-      )}
-    </ThemeConsumer>
+        <button className='btn-clear flex-center' onClick={onReset}>
+          <FaTimesCircle size={26} color='rgb(194,57,42)' />
+        </button>
+      </div>
+    </div>
   );
 }
 
