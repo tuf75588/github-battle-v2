50a91d61d72ea7d5d4d36403899c7a68053d546c
Andrew Davis <atd285@gmail.com>
2019-08-23 20:05:59 -0400

    replace hover render prop with custom hook

--- app/Components/Hover.js
+++ app/Components/Hover.js
@@ -3,25 +3,17 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-class Hover extends React.Component {
-  state = {
-    hovering: false,
-  };
-
-  mouseOut = () => {
-    this.setState({ hovering: false });
-  };
-
-  mouseOver = () => {
-    this.setState({ hovering: true });
-  };
-
-  render() {
-    return (
-      <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
-        {this.props.children(this.state.hovering)}
-      </div>
-    );
+export function useHover() {
+  const [hovering, setHovering] = React.useState(false);
+  function mouseOver() {
+    setHovering(true);
   }
+  function mouseOut() {
+    setHovering(false);
+  }
+  const attrs = {
+    onMouseOver: mouseOver,
+    onMouseOut: mouseOut,
+  };
+  return [hovering, attrs];
 }
-export default Hover;
